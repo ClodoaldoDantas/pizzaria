84aa6e12bcc72d7831eaac2bcb84cc76039e4855
clodoaldodantas <clodoaldodantas8@gmail.com>
2021-10-05 11:14:28 -0300

    Backend: Buscando pizza por id, atualizando e deletando pizza

--- server/src/controllers/PizzasController.ts
+++ server/src/controllers/PizzasController.ts
@@ -20,4 +20,35 @@ export class PizzasController {
     const pizzas = await PizzaModel.find();
     return response.json(pizzas);
   }
+
+  async findById(request: Request, response: Response) {
+    const { id } = request.params;
+    const pizza = await PizzaModel.findById(id);
+    return response.json(pizza);
+  }
+
+  async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const { name, description, price, active, image } = request.body;
+
+    const pizza = await PizzaModel.findByIdAndUpdate(
+      id,
+      {
+        name,
+        description,
+        price,
+        active,
+        image,
+      },
+      { new: true }
+    );
+
+    return response.json(pizza);
+  }
+
+  async delete(request: Request, response: Response) {
+    const { id } = request.params;
+    await PizzaModel.findByIdAndRemove(id);
+    return response.sendStatus(204);
+  }
 }
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,5 +6,8 @@ const pizzasController = new PizzasController();
 
 routes.post('/pizzas', pizzasController.create);
 routes.get('/pizzas', pizzasController.findAll);
+routes.get('/pizzas/:id', pizzasController.findById);
+routes.put('/pizzas/:id', pizzasController.update);
+routes.delete('/pizzas/:id', pizzasController.delete);
 
 export { routes };
