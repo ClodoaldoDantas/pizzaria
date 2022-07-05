d512345e7881a8c2adecf70826451748bea9fd22
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2022-07-05 00:26:10 -0300

    🐛 Corrigindo links ativos na sidebar

--- web/src/components/Sidebar/SidebarNav/index.tsx
+++ web/src/components/Sidebar/SidebarNav/index.tsx
@@ -1,36 +1,43 @@
+import cx from 'classnames';
+import { Link, useLocation } from 'react-router-dom';
 import { MdRestaurantMenu } from 'react-icons/md';
 import { FaClipboardList } from 'react-icons/fa';
-import { ActiveLink } from '../../ActiveLink';
 
 import './styles.scss';
 
-const navItems = [
-  {
-    label: 'Produtos',
-    link: '/',
-    icon: <MdRestaurantMenu size={20} />,
-  },
-  {
-    label: 'Pedidos',
-    link: '/orders',
-    icon: <FaClipboardList size={20} />,
-  },
-];
-
 export function SidebarNav() {
+  const { pathname } = useLocation();
+
+  const isHome = pathname === '/';
+  const productLinkIsActive = isHome || pathname.includes('/products');
+  const ordersLinkIsActive = pathname.includes('/orders');
+
   return (
     <>
       <ul className="nav nav-pills flex-column">
-        {navItems.map(navItem => (
-          <li key={navItem.label} className="nav-item">
-            <ActiveLink
-              label={navItem.label}
-              link={navItem.link}
-              icon={navItem.icon}
-              shouldMatchExactHref={navItem.label === 'Produtos'}
-            />
-          </li>
-        ))}
+        <li className="nav-item">
+          <Link
+            to="/"
+            className={cx('nav-link text-white', {
+              active: productLinkIsActive,
+            })}
+          >
+            <MdRestaurantMenu size={20} />
+            <span className="ms-2">Produtos</span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link
+            to="/orders"
+            className={cx('nav-link text-white', {
+              active: ordersLinkIsActive,
+            })}
+          >
+            <FaClipboardList size={20} />
+            <span className="ms-2">Pedidos</span>
+          </Link>
+        </li>
       </ul>
     </>
   );
