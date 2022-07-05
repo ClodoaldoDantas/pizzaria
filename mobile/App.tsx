830da0a6837ef46b81dac41858b91e888ab8191f
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2022-07-05 11:27:55 -0300

    ♻️  Alterando expo-app-loading por expo-splash-screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,36 +1,71 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
+import React, { useCallback, useEffect, useState } from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {
-  useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_600SemiBold,
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
 import { Routes } from './src/routes';
 import { OrdersProvider } from './src/contexts/OrdersContext';
 import { colors } from './src/theme/colors';
+import { View } from 'react-native';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Poppins_400Regular,
-    Poppins_500Medium,
-    Poppins_600SemiBold,
-    Poppins_700Bold,
-  });
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  if (!fontsLoaded) {
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Poppins_400Regular,
+          Poppins_500Medium,
+          Poppins_600SemiBold,
+          Poppins_700Bold,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
     return null;
   }
 
   return (
-    <NavigationContainer>
-      <OrdersProvider>
-        <Routes />
-        <StatusBar backgroundColor={colors.red_500} style="light" />
-      </OrdersProvider>
-    </NavigationContainer>
+    <View
+      onLayout={onLayoutRootView}
+      style={{
+        flex: 1,
+      }}
+    >
+      <NavigationContainer>
+        <OrdersProvider>
+          <Routes />
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+        </OrdersProvider>
+      </NavigationContainer>
+    </View>
   );
 }
